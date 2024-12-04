1edf1aa4d431b3760d11f702d37fa9dee17ab1e8
红色枫叶 <BlackMaple@live.cn>
2024-12-04 09:17:36 +0800

    1.modify png color

--- app/src/main/java/com/android/maple/ui/UIComponent.java
+++ app/src/main/java/com/android/maple/ui/UIComponent.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
 import com.android.maple.monodto.MonoResultDTO;
 import com.android.maple.service.IUIMessageHandler;
 import com.android.maple.service.MapleService;
@@ -15,15 +17,22 @@ public abstract class UIComponent implements IUIMessageHandler {
     private final UIMenuMain m_MenuMain;
     private final LinearLayout.LayoutParams m_ButtonLayoutParams;
 
-    public UIComponent(UIMenuMain menuMain) {
+    public UIComponent(@NonNull UIMenuMain menuMain) {
         this.m_MenuMain = menuMain;
         this.m_ButtonLayoutParams = createButtonLayoutParams(menuMain.getContext());
     }
 
-    private LinearLayout.LayoutParams createButtonLayoutParams(Context context) {
+    @NonNull
+    private LinearLayout.LayoutParams createButtonLayoutParams(@NonNull Context context) {
+
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        //    int buttonMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, displayMetrics);
+
+
         int buttonSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 64, displayMetrics);
         return new LinearLayout.LayoutParams(buttonSize, buttonSize);
+        //    layout.setMargins(buttonMargin,buttonMargin,buttonMargin,buttonMargin);
+        //  return  layout;
     }
 
 
@@ -53,7 +62,7 @@ public abstract class UIComponent implements IUIMessageHandler {
         Toast.makeText(this.getContext(), msg, Toast.LENGTH_LONG).show();
     }
 
-    protected void showError(MonoResultDTO dto) {
+    protected void showError(@NonNull MonoResultDTO dto) {
         showMsg(String.format("API ERROR:%s", dto.MSG));
     }
 }
