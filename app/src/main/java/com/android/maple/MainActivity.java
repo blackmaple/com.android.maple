5123191a1c34db7fde7a01453cec920fdaed741e
红色枫叶 <BlackMaple@live.cn>
2025-04-02 13:07:35 +0800

    1.wasm?

--- app/src/main/java/com/android/maple/MainActivity.java
+++ app/src/main/java/com/android/maple/MainActivity.java
@@ -1,6 +1,11 @@
 package com.android.maple;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -12,17 +17,42 @@ import com.android.maple.ui.UIResourceManager;
 public final class MainActivity extends AppCompatActivity {
 
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout linearLayout = UIResourceManager.createLayout(this);
-        ImageButton imageButton = UIResourceManager.createEditButton(this);
-        imageButton.setOnClickListener(v -> Toast.makeText(this.getBaseContext(), getApplication().getPackageName(), Toast.LENGTH_SHORT).show());
-        linearLayout.addView(imageButton);
-        setContentView(linearLayout);
-        mapleLauncher.show(this);
+//        LinearLayout linearLayout = UIResourceManager.createLayout(this);
+//        ImageButton imageButton = UIResourceManager.createEditButton(this);
+//        imageButton.setOnClickListener(v -> Toast.makeText(this.getBaseContext(), getApplication().getPackageName(), Toast.LENGTH_SHORT).show());
+//        linearLayout.addView(imageButton);
+//        setContentView(linearLayout);
+//        mapleLauncher.show(this);
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient()
+        {
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // 页面加载完成
+                super.onPageFinished(view, url);
+            }
+        });
+        webView.loadUrl("http://192.168.3.185:19139/index");
+        setContentView(webView);
     }
+
 
 }
 
